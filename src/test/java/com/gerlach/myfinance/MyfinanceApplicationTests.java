200f9916f5db073147c82c00c0e1a0e159fa399c
Jacob Gerlach <57069011+jwgerlach00@users.noreply.github.com>
2025-08-12 23:01:33 -0400

    Initial commit

---
+++ src/test/java/com/gerlach/myfinance/MyfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerlach.myfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
